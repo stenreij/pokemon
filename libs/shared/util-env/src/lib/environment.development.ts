20c683cd9ad9f77b85a7835d4ad219eaa25e349b
stenreij <74412704+stenreij@users.noreply.github.com>
2023-11-14 14:31:34 +0100

    lclapiurl --> onlineapiurl
    Changed value of onlineApiUrl to related url.

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -3,5 +3,5 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
     lclApiUrl: 'http://localhost:3000/api/team',
-    onlineApiUrl: 'http://localhost:3000/api/team',
+    onlineApiUrl: 'https://pokemon-cswf.azurewebsites.net/api/team',
 }
