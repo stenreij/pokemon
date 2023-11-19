a88e6902100f010bd6955c6e6662615af86ce1b8
stenreij <stenkap0113@gmail.com>
2023-11-19 12:31:57 +0100

    about component

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { TeamListComponent } from 'libs/frontend/features/src/lib/team/team-list/team-list.component';
 import { TeamDetailComponent } from 'libs/frontend/features/src/lib/team/team-detail/team-detail.component';
+import { AboutComponent } from 'libs/frontend/features/src/lib/about/about.component' 
 
 export const appRoutes: Routes= [
     { path:"", component: TeamListComponent },
     { path: "team/:id", component: TeamDetailComponent },
+    { path: "about", component: AboutComponent},
 ];
 
 @NgModule({
