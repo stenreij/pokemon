72f6b7ad84a6599271581d38b268bbef80ae24d9
stenreij <74412704+stenreij@users.noreply.github.com>
2023-12-03 20:44:39 +0100

    team entity
    connected team to mongoDB.

--- libs/backend/features/src/lib/team/team.controller.ts
+++ libs/backend/features/src/lib/team/team.controller.ts
@@ -1,36 +1,37 @@
 import { Controller, Delete, Put } from '@nestjs/common';
-import { TeamService } from './team.service';
+import { TeamService } from '../team/team.service';
 import { Get, Param, Post, Body } from '@nestjs/common';
 import { ITeam } from '@pokemon/shared/api';
-import { CreateTeamDto } from '@pokemon/backend/dto';
+import { CreateTeamDto, UpdateTeamDto } from '@pokemon/backend/dto';
 
 @Controller('team')
 export class TeamController {
     constructor(private teamService: TeamService) {}
 
     @Get('')
-    getAll(): ITeam[] {
-        return this.teamService.getAll();
+    async findAll(): Promise<ITeam[]> {
+        return this.teamService.findAll();
     }
 
     @Get(':id')
-    getOne(@Param('id') id: number): ITeam {
-        return this.teamService.getOne(id);
+    async findOne(@Param('id') id: number): Promise<ITeam | null> {
+        return this.teamService.findOne(id);
     }
 
     @Post('')
-    create(@Body() data: CreateTeamDto): ITeam {
-        return this.teamService.create(data);
+    async create(@Body() team: CreateTeamDto): Promise<ITeam> {
+        return this.teamService.create(team);
     }
 
     @Put(':id')
-    update(@Param('id') id: number, @Body() data: CreateTeamDto): ITeam {
-        return this.teamService.update(id, data);
-    }
-    
-    @Delete(':id')
-    delete(@Param('id') id: number): ITeam {
-        return this.teamService.delete(id);
+    async update(@Param('id') teamId: number,
+        @Body() data: UpdateTeamDto
+    ): Promise<ITeam | null> {
+        return this.teamService.update(teamId, data);
     }
 
+    @Delete(':id')
+    delete(@Param('id') teamId: number): Promise<ITeam | null> {
+        return this.teamService.delete(teamId);
+    }
 }
