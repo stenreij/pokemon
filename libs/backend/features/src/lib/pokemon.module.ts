e76e4f0d46f4cf432c219ff22eab87f0ee708e3e
stenreij <74412704+stenreij@users.noreply.github.com>
2023-12-04 17:15:26 +0100

    pokemon updated/deleted does cahnge team.pokemon[]
    If a pokemon gets updated, it also updates the pokemon in teams if its in a team.
    If pokemon gets deleted, it also gets deleted from all existing teams if the pokemon is part of team(s).
    
    twaeked clicks on button (was mostly on links and not the whole button)

--- libs/backend/features/src/lib/pokemon.module.ts
+++ libs/backend/features/src/lib/pokemon.module.ts
@@ -3,12 +3,16 @@ import { PokemonController } from './pokemon/pokemon.controller';
 import { PokemonService } from './pokemon/pokemon.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from './pokemon/pokemon.schema';
+import { TeamModule } from './team.module';
+import { Team, TeamSchema } from './team/team.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Pokemon.name, schema: PokemonSchema }
-    ])
+      { name: Pokemon.name, schema: PokemonSchema },
+      { name: Team.name, schema: TeamSchema}
+    ]),
+    TeamModule,
   ],
   controllers: [PokemonController],
   providers: [PokemonService],
--- libs/backend/features/src/lib/pokemon/pokemon.service.ts
+++ libs/backend/features/src/lib/pokemon/pokemon.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { IPokemon } from '@pokemon/shared/api';
 import { CreatePokemonDto, UpdatePokemonDto } from '@pokemon/backend/dto';
 import { Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Pokemon as PokemonModel, PokemonDocument } from './pokemon.schema';
+import { Team as TeamModel, TeamDocument } from '../team/team.schema';
 
 @Injectable()
 export class PokemonService {
@@ -12,7 +13,8 @@ export class PokemonService {
     private readonly logger: Logger = new Logger(PokemonService.name);
 
   constructor(
-        @InjectModel(PokemonModel.name) private pokemonModel: Model<PokemonDocument>
+        @InjectModel(PokemonModel.name) private pokemonModel: Model<PokemonDocument>,
+        @InjectModel(TeamModel.name) private teamModel: Model<TeamDocument>,
         ) {}
 
     async findAll(): Promise<IPokemon[]> {
@@ -34,18 +36,28 @@ export class PokemonService {
         this.logger.log(`create(${pokemon})`);
         const lowestAvailableId = await this.getLowestAvailablePokemonId();
         const pokemonWithId = { ...pokemon, pokemonId: lowestAvailableId };
-        const createdItem = this.pokemonModel.create(pokemonWithId);
+        const createdItem = await this.pokemonModel.create(pokemonWithId);
         return createdItem;
     }
 
     async update(pokemonId: number, pokemon: UpdatePokemonDto): Promise<IPokemon | null> {
         this.logger.log(`Update pokemon ${pokemon.name}`);
-        return this.pokemonModel.findOneAndUpdate({ pokemonId }, pokemon);
+        const updatedPokemon = await this.pokemonModel.findOneAndUpdate({ pokemonId }, pokemon, { new: true });
+        if (!updatedPokemon) {
+            return null; 
+        }
+
+        await this.updatePokemonInTeams(updatedPokemon);
+        return updatedPokemon;
     }
 
     async delete(pokemonId: number): Promise<IPokemon | null> {
         this.logger.log(`Delete pokemon ${pokemonId}`);
         const deletedItem = await this.pokemonModel.findOneAndDelete({ pokemonId }, {}).exec();
+        if (deletedItem) {
+            await this.removePokemonFromTeams(deletedItem);
+        }
+    
         return deletedItem;
     }
 
@@ -56,5 +68,45 @@ export class PokemonService {
             lowestId++;
         }
         return lowestId;
+    }
+
+    private async updatePokemonInTeams(pokemon: IPokemon): Promise<void> {
+        const teamsContainingPokemon = await this.teamModel.find({ 'pokemon.pokemonId': pokemon.pokemonId }).exec();
+
+        if (teamsContainingPokemon.length === 0) {
+            return; 
+        }
+
+        await Promise.all(
+            teamsContainingPokemon.map(async (team) => {
+                const updatedTeam = team.toObject();
+                const index = updatedTeam.pokemon.findIndex((p) => p.pokemonId === pokemon.pokemonId);
+
+                if (index !== -1) {
+                    updatedTeam.pokemon[index] = { ...pokemon }; 
+                    await this.teamModel.findOneAndUpdate({ teamId: team.teamId }, updatedTeam);
+                }
+            })
+        );
+    }
+
+    private async removePokemonFromTeams(pokemon: IPokemon): Promise<void> {
+        const teamsContainingPokemon = await this.teamModel.find({ 'pokemon.pokemonId': pokemon.pokemonId }).exec();
+    
+        if (teamsContainingPokemon.length === 0) {
+            return;
+        }
+    
+        await Promise.all(
+            teamsContainingPokemon.map(async (team) => {
+                const updatedTeam = team.toObject();
+                const index = updatedTeam.pokemon.findIndex((p) => p.pokemonId === pokemon.pokemonId);
+    
+                if (index !== -1) {
+                    updatedTeam.pokemon.splice(index, 1);
+                    await this.teamModel.findOneAndUpdate({ teamId: team.teamId }, updatedTeam);
+                }
+            })
+        );
     }
 }
