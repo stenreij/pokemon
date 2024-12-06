96c6834b49b4708235d09c331261fe4422985e14
stenreij <74412704+stenreij@users.noreply.github.com>
2024-12-06 15:18:27 +0100

    endpoint update

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
     lclApiUrl: 'http://localhost:3000/api',
-    onlineApiUrl: 'https://pokemon-cswf.azurewebsites.net/',
+    onlineApiUrl: 'https://pokemon-cswf.azurewebsites.net',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://sten:Anstjo94!@pokemon.disyksv.mongodb.net/',
 
 }
