9b9d364a540651aab0e479239b6a3c9bf1f3829d
stenreij <74412704+stenreij@users.noreply.github.com>
2025-03-25 13:15:09 +0100

    Endpoint update
    Switched offline and online endpoints so it works local.

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//import { environment } from '@pokemon/shared/util-env'
+//import { Neo4jBackendModule } from '@pokemon/backend/neo4j'
+//import { Neo4jModule } from 'nest-neo4j/dist'
 
 @Module({
-  imports: [],
+  imports: [
+    //Neo4jModule.forRoot({
+      //scheme: 'neo4j+s',
+      //host: environment.NEO4J_DB_HOST,
+      //port: environment.NEO4J_DB_PORT,
+      //username: environment.NEO4J_DB_USER,
+      //password: environment.NEO4J_DB_PASSWORD,
+    //}),
+    //Neo4jBackendModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,7 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
     lclApiUrl: 'http://localhost:3000/api',
-    onlineApiUrl: 'https://pokemon-cswf.azurewebsites.net',
+    //onlineApiUrl: 'https://pokemon-cswf.azurewebsites.net',
+    onlineApiUrl: 'http://localhost:3000/api',
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://sten:Anstjo94!@pokemon.disyksv.mongodb.net/',
 
 }
